1a3570e86711a0180c780904aa9abe3e34afdc04
Arun jyothish <arunjyothishvikku@gmail.com>
2022-05-02 12:47:58 +0530

    Add in Workout table

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
+# RandomWord
+
+Enter details of a person. It stores in a database. whenever the date of birth of a person is upcoming
+an alert will be pop up on computer screen.
+
+init.rc run.
+
+birthday alert
+
 ```mermaid
-
 flowchart TD
-    a-->b
-
-```
+    START((START))-->DISPLAY(DISPLAY:  \n DATE \n TIME \n DATABASE STATUS)
+    DISPLAY-->MENU(PROMPT OPTIONS)
+    MENU{MENU}-->INSERT(INSERT A PERSON INTO DB)
+    MENU-->DSP_AV(DISPLAY_AVAILABLE_PERSON)
+    MENU-->SHOW_HIERARCHY
+    INSERT-->PROMPT(PROMPT PERSONAL DETAILS)
+    DSP_AV-->FETCH(FETCH PERSONAL\n DETAILS FROM PERSON DATABASE)
+    SHOW_HIERARCHY-->B'DAY(SHOW_HIERARCHY BASED \n ON THEIR BIRTH DATE COMING)
+``` 
--- main.py
+++ main.py
@@ -9,26 +9,56 @@ import readchar
 import requests
 
 mySQLdb = mysql.connector.connect(
-    host="localhost",
-    user="guest",
-    password="guest123",
-    database="mec")
+    host="localhost",  # server address
+    user="guest",  # user name for database
+    password="guest123",  # password for database
+    database="mec")  # mec database
+
+
+def insertIntoDB(name, dob):
+    val = (name, dob)
+    command = "INSERT INTO FRIENDS (NAME,DATE_OF_BIRTH) VALUES (%s, %s)"
+    myCursor.execute(command, val)
+
 
 print(mySQLdb)
-mycursor = mySQLdb.cursor()
-dbs = mycursor.execute("show tables")
-print(dbs)
+print("\n")
+myCursor = mySQLdb.cursor()
+dbs = myCursor.execute("SELECT * FROM friends")  # friends table
+for x in myCursor.fetchall():
+    print(x)
+
+print("\n")
+print("\n")
+print("mysql")
+print("\n")
 
 
 # PERSON CLASS
 class Persons:
-    def __init__(self, name, gender, dob):
+    # parent class
+    def __init__(self, fname, lname, gender, dob):
         self.dob = dob
-        self.name = name
         self.gender = gender
+        self.firstName = fname
+        self.lastName = lname
 
-    def getname(self):
-        return self.name
+    def getName(self):
+        name = self.firstName + " " + self.lastName
+        print(name)
+        return name
+
+    def getDOB(self):
+        print(self.dob)
+
+
+class Customer:
+    def __init__(self, fname, lname):
+        self.firstName = fname
+        self.lastName = lname
+
+    def printName(self):
+        print(self.firstName, self.lastName)
 
 
 # INHERITING PERSON CLASS FOR EMPLOYEES
@@ -40,7 +70,7 @@ class Employee:
 
 # RANDOM WORDS FOR DOMAIN SEARCH
 class RandomWord:
-    def __init__(self, noVowel=None):
+    def __init__(self, noVowel):
         vowels = ['a', 'e', 'i', 'o', 'u']
         consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'v', 'w',
                       'x', 'y', 'z']
@@ -66,14 +96,14 @@ def clearConsole():
 
 # GOOGLE DOMAIN SEARCH AUTOMATED DOMAIN SEARCH
 def urlRequest():
-    domain = "wtfnoway"
+    domain = "text"  # sample string
     url = f"https://domains.google.com/registrar/search?searchTerm={domain}"
     r = requests.get(url)
     print(r.text)
 
 
 print("press ENTER to start program")
-while (True):
+while True:
     # IF THE SCRIPT DIRECTLY EXECUTED (UNLESS IMPORTED) EXECUTING THE FOLLOWING
     if __name__ == "__main__":
         today = datetime.datetime.today().strftime("%c")
@@ -81,9 +111,19 @@ while (True):
         # for i in range (0,9):
         # RandomWord(3)
         print(sys.path[0])
+    print("\n")
+    print("\n")
+    new = Persons(input("your firstName: "), input("your lastname: "), input("gender: "), input("date of birth: "))
 
-    print("OPTIONS: [xx-quit] [rw-random word]")
+    print("\n")
+    print("\n")
+    new.getName()
+    new.getDOB()
+
+    print("\n")
+    print("\n")
     print("Enter Key ..")
+    print("OPTIONS: [xx-quit] [rw-random word] [in- insert into database]")
     firstStroke = readchar.readchar()
     clearConsole()
     secondStroke = readchar.readchar()
@@ -93,4 +133,11 @@ while (True):
             break
     elif firstStroke == 'r':
         if secondStroke == 'w':
-            RandomWord()
+            RandomWord(3)
+    if firstStroke == 'u':
+        if secondStroke == 'r':
+            urlRequest()
+    if firstStroke == 'i':
+        if secondStroke == 'n':
+            pass
+    #            insertIntoDB(new.getName(),new.getDOB())
